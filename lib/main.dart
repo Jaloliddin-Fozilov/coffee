a76a626653a2a61879ce292351a0635bd7a006a4
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-08-30 16:45:48 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Coffee App',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.format_align_left),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_basket_outlined),
+          ),
+        ],
+      ),
+      body: Expanded(
+        child: Stack(
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).size.height,
+              child: Container(
+                color: Colors.amber,
+                height: 300,
+                width: double.infinity,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
