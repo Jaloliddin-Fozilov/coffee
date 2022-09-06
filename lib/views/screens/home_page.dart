ed7f7f7c5418a365bcf4fbfd41d2116e0becbe50
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-09-06 22:26:06 +0500

    home page finished

---
+++ lib/views/screens/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:coffee/views/widgets/home_header_with_appbar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          HomeHeaderWithAppBar(),
+          Expanded(
+            child: ListView(
+              children: const [
+                Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                    ),
+                    title: Text('Pink in disguise'),
+                    subtitle: Text('\$3.55'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1535568824865-a801351e8483?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                    ),
+                    title: Text('baked blueberry'),
+                    subtitle: Text('\$3.55'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1618436784394-f5199d65b8db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+                    ),
+                    title: Text('Glaze party'),
+                    subtitle: Text('\$3.55'),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
